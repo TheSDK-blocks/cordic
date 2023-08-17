c0f052654354dcd22a2374833000c738121dcc0a
Aleksi Korsman <aleksi.korsman@aalto.fi>
2023-08-17 16:36:32 +0300

    Move types into own file

---
+++ general_cordic/cordic_types.py
@@ -0,0 +1,17 @@
+class rotation_type:
+    """
+    CIRCULAR, LINEAR, or HYPERBOLIC
+    """
+
+    CIRCULAR = 0
+    LINEAR = 1
+    HYPERBOLIC = 2
+
+
+class cordic_mode:
+    """
+    ROTATION or VECTORING
+    """
+
+    ROTATION = 0
+    VECTORING = 1
