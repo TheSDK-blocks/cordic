d09741e731cec34dc2523c262f970fe8ca81ad7a
Aleksi Korsman <aleksi.korsman@aalto.fi>
2023-10-11 09:03:12 +0300

    Calculate accurate values for atan and atanh

--- cordic_common/cordic_constants.py
+++ cordic_common/cordic_constants.py
@@ -1,26 +1,11 @@
-atan_lut = [
-    0.7853981634,
-    0.463647609,
-    0.2449786631,
-    0.1243549945,
-    0.06241881,
-    0.03123983343,
-    0.01562372862,
-    0.00781234106,
-    0.003906230132,
-]
+from math import atan, atanh
 
-atanh_lut = [
-    None,  # +inf
-    0.549306144334,
-    0.255412811883,
-    0.12565721414,
-    0.062581571477,
-    0.031260178491,
-    0.015626271752,
-    0.007812658952,
-    0.003906269868,
-]
+iters = 30
+
+atan_lut = [atan(2**-i) for i in range(0, iters)]
+
+# atanh(1) is inf
+atanh_lut = [None] + [atanh(2**-i) for i in range(1, iters)]
 
 # Hyperbolic mode does not converge without
 # repeating some iterations
